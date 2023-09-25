5974e2bb8c22d98d476e825a1815647678c1b6a1
Chanelle Allen <chanelle3allen@gmail.com>
2023-09-25 00:58:28 -0700

    Changed function of buttons

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const img = document.getElementById('Cover'),
-title = document.getElementById('msuic-title'),
-artist = document.getElementById('music-artist'),
-currentTimeEl = document.getElementById('current-time'),
-durationEl = document.getElementById('duration'),
-progress = document.getElementById('progress'),
-playerProgress = document.getElementById('player-progress'),
-prevBtn = document.getElementById('prev'),
-nextBtn = document.getElementById('next'),
-playBtn = document.getElementById('play'),
-background = dodcument.getElementById('bg-img');
 
-const music = new Audio()
-
-const songs = [
-    {
-        path: 'images/lilbaby-scarred',
-        displayName: 'Emotionally Scarred',
-        cover: 'images/lilbaby-scarred',
-        artist: 'Lil Baby',
-    },
-]
 
 
